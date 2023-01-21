00efec28d9999f25ab5dc47bf4c00dc8f439fe5e
webmaster019 <kingbsetd030@gmail.com>
2023-01-21 09:24:32 +0200

    Fix footer alignement

--- src/components/layouts/footer/index.js
+++ src/components/layouts/footer/index.js
@@ -7,32 +7,26 @@ const Footer=()=>{
     return(
         <div className="footer">
             <Container>
-                <div className="row pt-5 pb-3  ">
+                <div className="row pt-3 pb-3 d-flex align-items-center ">
                     <div className="col-12 col-xl-3 col-lg-3
                      align-items-center
                     justify-content-start "
                     >
-                            <Link href={"/"} className="logo-a">
-                                <Image
-                                    src={"/assets//logos/3.png"}
-                                    alt="cover"
-                                    className="logo-image-footer"
-                                    width={50}
-                                    height={50} />
-                            </Link>
+                        <Link href={"/"} className="logo-a">
+                            <Image
+                                src={"/assets//logos/3.png"}
+                                alt="cover"
+                                className="logo-image-footer"
+                                width={50}
+                                height={50} />
+                        </Link>
                     </div>
-
-
-                </div>
-                <hr className="text-white"/>
-                <div className="row pt-3 pb-3 d-flex align-items-center ">
-                    <div className="col-12 col-lg-6 col-xl-6 justify-content-start align-content-start ">
+                    <div className="col-12 col-lg-4 col-xl-4 d-flex justify-content-center align-content-center ">
                         <p className="text-white">© {new Date().getFullYear()} <strong> RemoteLabs</strong> -All rights reserved </p>
                     </div>
                     <div
-                        className="col-12 col-lg-6 col-xl-6 justify-content-between   align-content-center text-center d-flex gap-5">
-                        <div className="align-self-start">Termes Conditions</div>
-                        <div className="align-self-end">Confidentialite</div>
+                        className="col-12 col-lg-4 col-xl-4 justify-content-end   align-content-end text-end d-flex gap-5">
+                        <div className="align-self-start">Termes & Conditions</div>
                     </div>
 
                 </div>
