aeb6ed278a6c61a8b8bf9b8fa920b89a0d5b4653
webmaster019 <kingbsetd030@gmail.com>
2023-01-22 14:17:20 +0200

    fix Meta tags image url

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,9 +10,10 @@ export default function App({ Component, pageProps }) {
   return (
       <>
           <Head>
+              <meta property="og:url" content="https://hire-stage.vercel.app" />
               <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <meta property="og:image"  content="/assets/logos/2.jpeg"/>
-              <meta name="twitter:image" content="/assets/logos/2.jpeg"/>
+              <meta property="og:image"  content="https://hire-stage.vercel.app/assets/logos/2.jpg"/>
+              <meta name="twitter:image" content="https://hire-stage.vercel.app/assets/logos/2.jpg"/>
           </Head>
         <SiteLayout>
           <Component {...pageProps} />
