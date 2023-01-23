3cf45319aa69d5003ad7514834c7d17e843a9e76
webmaster019 <kingbsetd030@gmail.com>
2023-01-23 02:10:35 +0200

    Fix Hire  frontend technologies list review 1

--- src/components/elements/home/HomeCover.js
+++ src/components/elements/home/HomeCover.js
@@ -5,16 +5,16 @@ import Image from "next/image";
 const HomeCover = () => {
 
     return(
-        <div className="home-cover flex-mobile container ">
-            <Row className="container-fluid-sm">
-                <div className="col-12 col-lg-3 col-xl-3 right-section-header  d-flex pt-xl-2 pt-lg-2 ">
+        <div className="home-cover  container-fluid px-5 ">
+            <Row className="justify-content-between">
+                <div className="col-12 col-lg-3 col-xl-3 left-section-header  d-flex pt-xl-2 pt-lg-2 pb-0 pb-md-4">
 
                     <Image
                         src="/assets/images/1c.png"
                         alt="cover"
-                        className="cover_home  "
+                        className="cover_home "
                         width={700}
-                        height={70}
+                        height={700}
                     />
                 </div>
                 <div className="col-12 col-lg-6 col-xl-6 mt-5 mt-xl-0 mt-lg-0  d-flex flex-column align-items-center justify-content-center pt-xl-2 pt-lg-2 ">
@@ -23,14 +23,14 @@ const HomeCover = () => {
                     <h3 className="text-cover-home text-site2">with RemoteLabs</h3>
 
                 </div>
-                <div className="col-12 col-lg-3 col-xl-3 right-section-header  d-none d-xl-flex d-lg-flex pt-xl-2 pt-lg-2 ">
+                <div className="col-12 col-lg-3 col-xl-3 right-section-header  d-none d-xl-flex d-lg-flex pt-xl-2 pt-lg-2 justify-content-end   ">
 
                     <Image
-                        src="/assets/images/2c.png"
+                        src="/assets/images/5c.png"
                         alt="cover"
                         className="cover_home  "
                         width={700}
-                        height={70}
+                        height={700}
                     />
                 </div>
             </Row>
